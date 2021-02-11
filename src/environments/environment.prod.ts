745e70bd5d14af277e21b6c59e385fbdd9091f35
AnaPaixao <61764104+AnaPaixao@users.noreply.github.com>
2021-02-10 21:56:09 -0300

    adicionando o firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyDaQCFjHTDw8pIOQaiQmD2e3G-mYUuabnY",
+    authDomain: "projeto-integrador-ionic-a6eab.firebaseapp.com",
+    projectId: "projeto-integrador-ionic-a6eab",
+    storageBucket: "projeto-integrador-ionic-a6eab.appspot.com",
+    messagingSenderId: "751793583158",
+    appId: "1:751793583158:web:c0d39e7bde6630821e9497"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyDaQCFjHTDw8pIOQaiQmD2e3G-mYUuabnY",
+    authDomain: "projeto-integrador-ionic-a6eab.firebaseapp.com",
+    projectId: "projeto-integrador-ionic-a6eab",
+    storageBucket: "projeto-integrador-ionic-a6eab.appspot.com",
+    messagingSenderId: "751793583158",
+    appId: "1:751793583158:web:c0d39e7bde6630821e9497"
+  }
 };
 
 /*
